7a3b1a8e7c89f796af3210459cecf3fd7bf01dcc
Russ DEV <niahui.dev25@gmail.com>
2025-06-09 10:54:54 -0500

    Leading y Actions implementados

--- lib/presentation/delegates/search_movie_delegate.dart
+++ lib/presentation/delegates/search_movie_delegate.dart
@@ -1,6 +1,8 @@
+import 'package:animate_do/animate_do.dart';
+import 'package:cinamapedia/domain/entities/movie.dart';
 import 'package:flutter/material.dart';
 
-class SearchMovieDelegate extends SearchDelegate {
+class SearchMovieDelegate extends SearchDelegate<Movie?> {
   SearchMovieDelegate()
     : super(
         searchFieldLabel: 'Buscar películas',
@@ -10,25 +12,27 @@ class SearchMovieDelegate extends SearchDelegate {
   @override
   List<Widget> buildActions(BuildContext context) {
     return [
-      // IconButton(
-      //   icon: const Icon(Icons.clear),
-      //   onPressed: () {
-      //     query = '';
-      //   },
-      // ),
-      Text('Buiding actions...'),
+      //if (query.isNotEmpty)
+      FadeIn(
+        animate: query.isNotEmpty,
+        child: IconButton(
+          icon: const Icon(Icons.clear),
+          onPressed: () {
+            query = '';
+          },
+        ),
+      ),
     ];
   }
 
   @override
   Widget? buildLeading(BuildContext context) {
-    return Text('Buiding buildLeading...');
-    // IconButton(
-    //   icon: const Icon(Icons.arrow_back),
-    //   onPressed: () {
-    //     close(context, null);
-    //   },
-    // );
+    return IconButton(
+      icon: const Icon(Icons.arrow_back),
+      onPressed: () {
+        close(context, null);
+      },
+    );
   }
 
   @override
