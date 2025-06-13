053ee3053552ecb50a323dccec2dd239ff0d8d2c
Russ DEV <niahui.dev25@gmail.com>
2025-06-12 20:24:25 -0500

    agregando la carpeta assest y realizando las configraciones para la pantalla de categorias

--- lib/config/helpers/human_formats.dart
+++ lib/config/helpers/human_formats.dart
@@ -2,12 +2,16 @@ import 'package:intl/intl.dart';
 
 class HumanFormats {
   static String number(double number, [int decimalDigits = 0]) {
-    final formatterNumber = NumberFormat.compactCurrency(
-      decimalDigits: 0,
+    return NumberFormat.compactCurrency(
+      decimalDigits: decimalDigits,
       symbol: '',
       locale: 'en',
     ).format(number);
+  }
 
-    return formatterNumber;
+  static String shortDate(DateTime date) {
+    final format = DateFormat.yMMMEd('es');
+
+    return format.format(date);
   }
 }
